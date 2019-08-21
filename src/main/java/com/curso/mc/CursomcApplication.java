a4f0bff2a426e2e6f21b65212a6571084f56a144
Sidnei Quinelato <sidneiq@gmail.com>
2019-08-21 16:59:56 -0300

    criando servico categaria

--- src/main/java/com/curso/mc/CursomcApplication.java
+++ src/main/java/com/curso/mc/CursomcApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication {
 
+	//https://github.com/acenelio/springboot2-ionic-backend
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
